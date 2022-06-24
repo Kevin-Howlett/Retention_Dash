45be7de33e2fa009779181c6d48cb51baaa19c7d
palazzojulian <50282600+palazzojulian@users.noreply.github.com>
2022-06-24 13:11:43 -0700

    Debugging error handling

--- app.py
+++ app.py
@@ -1194,10 +1194,32 @@ def treatoutliers(df, columns=None, factor=3, method='IQR', treament='cap'):
 # CODE FOR REDIRECTING TERMINAL OUTPUT
 from contextlib import contextmanager
 from io import StringIO
-from streamlit.report_thread import REPORT_CONTEXT_ATTR_NAME
 from threading import current_thread
 import streamlit as st
 import sys
+
+# Ref: https://gist.github.com/tvst/036da038ab3e999a64497f42de966a92
+try:
+    from streamlit.scriptrunner import get_script_run_ctx
+except ModuleNotFoundError:
+    # streamlit < 1.8
+    try:
+        from streamlit.script_run_context import get_script_run_ctx  # type: ignore
+    except ModuleNotFoundError:
+        # streamlit < 1.4
+        from streamlit.report_thread import (  # type: ignore
+            get_report_ctx as get_script_run_ctx,
+        )
+
+from streamlit.server.server import Server
+
+def get_session_id() -> str:
+    ctx = get_script_run_ctx()
+    if ctx is None:
+        raise Exception("Failed to get the thread context")
+
+    return ctx.session_id
+
 
 
 @contextmanager
